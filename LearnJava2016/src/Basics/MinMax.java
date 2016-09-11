3430d3264f7d1419701f703edfe65f78dc4b001b
jacob <jacob@Skywalker>
2016-09-10 22:24:20 -0700

    Added more numbers

--- LearnJava2016/src/Basics/MinMax.java
+++ LearnJava2016/src/Basics/MinMax.java
@@ -4,7 +4,7 @@ public class MinMax
 {
 	public static void main(String[] args)
 	{
-		int[] arrayOfNumbers = {5, 10, 38, 4, 7, 23, 45, 65, 78, 97};
+		int[] arrayOfNumbers = {5, 10, 38, 4, 7, 23, 45, 65, 78, 97, 34, 25, 67, 101};
 		int largest = arrayOfNumbers[0];
 		int smallest = arrayOfNumbers[0];
 		for(int i = 0; i < arrayOfNumbers.length; i++)
