f283958e0f4b15b114aac5c17693f9fc38420156
jacob <jacob@Skywalker>
2016-09-10 21:20:56 -0700

    Added code

--- LearnJava2016/src/Basics/MinMax.java
+++ LearnJava2016/src/Basics/MinMax.java
@@ -2,5 +2,22 @@ package Basics;
 
 public class MinMax
 {
-
+	public static void main(String[] args)
+	{
+		int[] arrayOfNumbers = {5, 10, 38, 4, 7, 23, 45, 65, 78, 97};
+		int largest = arrayOfNumbers[0];
+		int smallest = arrayOfNumbers[0];
+		for(int i = 0; i < arrayOfNumbers.length; i++)
+		{
+			int n = arrayOfNumbers[i];
+			if(n < smallest)
+			{
+				smallest = n;
+			}
+			else if(n > largest)
+			{
+				largest = n;
+			}
+		}
+	}
 }
